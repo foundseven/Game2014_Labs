336af167d49423f8036809ff426d9dfd98a56864
Emma <pawful@bell.net>
2024-12-01 18:18:41 -0500

    added player LOS finished half of lab

---
+++ Game2014_Lab5/Assets/Scripts/EnemyBehaviour/PlayerDetection.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerDetection : MonoBehaviour
+{
+    [SerializeField]
+    bool _isSensing;
+
+    [SerializeField]
+    bool _LOS;
+
+    PlayerBehaviour _player;
+
+    [SerializeField]
+    LayerMask _layerMask;
+
+    void Start()
+    {
+        _player = FindObjectOfType<PlayerBehaviour>();
+    }
+
+    void Update()
+    {
+        if(_isSensing)
+        {
+            RaycastHit2D hit = Physics2D.Linecast(transform.position, _player.transform.position, _layerMask);
+
+            Vector2 playerDirection = _player.transform.position - transform.position;
+            float playerDirectionValue = (playerDirection.x > 0) ? 1 : -1;
+            float enemyLookingDirectionValue = (transform.localScale.x > 0) ? -1 : 1;
+
+            _LOS = (hit.collider.name == "Player") && playerDirectionValue == enemyLookingDirectionValue;
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.CompareTag("Player"))
+        {
+            _isSensing = true;
+        }
+    }
+
+    private void OnDrawGizmos()
+    {
+        Color color = (_LOS) ? Color.green : Color.red;
+
+        if(_isSensing)
+        {
+            Debug.DrawLine(transform.position, _player.transform.position, color);
+        }
+    }
+}
